a76668b6b175e145b77a67ff7a3a5e9f145d7f72
Marko <baghashvili.marko@gmail.com>
2024-08-05 00:59:22 +0400

    Update create-env.js

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,21 +1,16 @@
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
 
-try {
-  // Define the path to the .env file at the top level
-  const envFilePath = path.join(__dirname, "../.env");
+// Define the path to the .env file at the top level
+const envFilePath = path.join(__dirname, "../.env");
 
-  // Prepare environment variables
-  const envVariables = `
+// Prepare environment variables
+const envVariables = `
 VITE_APP_ID=${process.env.REACT_APP_APP_ID}
 VITE_FIREBASE_API_KEY=${process.env.REACT_APP_FIREBASE_API_KEY}
 `;
 
-  // Write the environment variables to the .env file
-  fs.writeFileSync(envFilePath, envVariables);
-  console.log(".env file created with the following content:");
-  console.log(envVariables);
-} catch (error) {
-  console.error("Error writing .env file:", error);
-  process.exit(1); // Exit with non-zero status code to indicate failure
-}
+// Write the environment variables to the .env file
+fs.writeFileSync(envFilePath, envVariables);
+console.log(".env file created with the following content:");
+console.log(envVariables);
